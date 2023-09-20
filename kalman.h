9d71662fe529f6ce93c581478b00db2cc89951e1
Abhi Arya <89806154+abhi-arya1@users.noreply.github.com>
2023-09-19 17:32:40 -0700

    remove unused for memory

--- kalman.h
+++ kalman.h
@@ -2,12 +2,6 @@
 #define KALMAN_H
 
 #include <Arduino.h>
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <sstream>
-#include <vector>
-#include <fstream>
 
 // U being the noisy measurement, and U_hat being the filtered estimate 
 float KALMAN_Altitude(float U);
