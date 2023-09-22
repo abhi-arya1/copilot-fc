412d78187f48071f8220abb605615771ed057da2
Abhi Arya <89806154+abhi-arya1@users.noreply.github.com>
2023-09-22 11:01:29 -0700

    pre-teensy switch prototyping and upgrades

--- flightComputer.ino
+++ flightComputer.ino
@@ -11,6 +11,15 @@
 Adafruit_MPL3115A2 baro;
 Adafruit_ICM20948 icm;
 
+KALMAN altitudeFilter(1, 1, 0.04); 
+MVAVG altFilter(5); 
+
+// sensor information (IMU)
+#define GYRO_DATA_RATE_HZ 100
+#define GYRO_DR_DIVISOR 10
+#define ACCEL_DATA_RATE_HZ 53.57
+#define ACCEL_DR_DIVISOR 20
+
 File altitude_and_pressure;
 File speed_and_accel;
 File gyro_angles;
@@ -59,9 +68,10 @@ void setup() {
     print("Altimeter Not Found");
     while(1);
   }
-  print("Altimeter found!");
   baro.setMode(MPL3115A2_ALTIMETER);
   baro.setSeaPressure(BARO_ZERO_PRESSURE);
+  altFilter.begin(); 
+  print("Altimeter setup complete!");
 
   // ICM_20948X
   if (!icm.begin_I2C()) { 
@@ -69,15 +79,32 @@ void setup() {
       print("Failed to find IMU");
       while (1);
     }}
-  print("IMU found!");
-  // TODO get IMU sensors calibrated with proper data ranges 
+  // TODO get IMU sensors calibrated with proper data ranges (FINISH THIS FIRST) 
+  icm.setAccelRange(ICM20948_ACCEL_RANGE_16_G);
+  icm.setGyroRange(ICM20948_GYRO_RANGE_2000_DPS);
+  icm.setMagDataRate(AK09916_MAG_DATARATE_100_HZ);
+  print("IMU setup and calibration complete!");
+
+
+  // Setup Pyros ---------------
+  if(COMP_MODE != 0) {
+    pinMode(PYRO_CHUTE, OUTPUT); 
+    print("Computer is: 'ARMED' on chute pyro channel."); 
+    if(PYRO_CHUTE_BACKUP_EN) {
+      pinMode(PYRO_CHUTE_BACKUP, OUTPUT); 
+      print("Computer is: 'ARMED' on chute backup pyro channel."); 
+    }
+    print("Pyro Channel(s) Set Up");
+  } else {
+    print("COMP_MODE = 0: Computer is 'DISARMED' on all Pyro Channels");
+  }
 
   // Setup Cooldown Sequence ---------------
   delay(750);
   redOff();
   print("Setup Complete");
   print("----------------------------");
-  blink(GREEN, 2, 300); 
+  blink(GREEN, 3, 250); 
   delay(250); 
 
     // Ready for flight! ---------------
@@ -89,9 +116,9 @@ void loop() {
   //float time = millis()/1000.0; // (for datalogging)
   //Serial.print("Flight Mode: "); Serial.println(flightMode); // (for testing)
 
-  // TODO complete pyro test section (E-match units)
+  // TODO complete pyro test section (E-match units/transistors and power delivery)
   // if(COMP_MODE == 1) {
-  //   
+    
   // } 
   
 
@@ -108,20 +135,22 @@ void loop() {
     case PAD: 
     { pinkOn(); 
         
+      // detect a potential launch 
       if(LAUNCH_METHOD == 0) 
-      { // detect a potential launch 
+      {
         // (TODO combine launch detect algo with accelerometer)
-        float altitude = KALMAN_Altitude(baro.getAltitude());
-        Serial.print(altitude); Serial.print(", "); Serial.println(initialAltitude); 
+        float altitude = baro.getAltitude();
+        float adjAltitude = altFilter.reading(altitude); 
+        Serial.print(adjAltitude); Serial.print(", "); Serial.print(altitude); Serial.print(", "); Serial.println(initialAltitude); 
 
         // set a baseline altitude
         if (launchCheckZero <= 5) { 
-          initialAltitude = baro.getAltitude(); 
+          initialAltitude = altitude; 
           launchCheckZero++; 
         }
 
         // detect a launch (TODO combine with accelerometer data for accurancy)
-        if ((altitude - 1) > initialAltitude) { 
+        if ((adjAltitude - 1) > initialAltitude) { 
           flightMode = ASCENT; pinkOff(); 
           print("LAUNCH DETECTED, Flight Mode Changed to *ASCENT* (2) from *PAD* (1)");
         }
@@ -132,15 +161,21 @@ void loop() {
       break; 
     } // TODO finish stage handling 
     case ASCENT:
-      blueOn(); 
+      yellowOn(); 
 
       // TODO data-logging and other ascent related features 
       
       break; 
     case DESCENT_RECOVERY: 
-      // TODO parachute handling 
+      blueOn(); 
+
+      if(/*condition to fire pyro channels is*/ true && COMP_MODE != 0){
+        // TODO add pyro support with power and transistors 
+      }
+
       break;
     case LANDED: 
+      greenOn(); 
       // TODO celebrate! (also make sure SD files are saved and the computer is yk...alive)
       break;
   }
@@ -152,6 +187,17 @@ void loop() {
     sensors_event_t gyro;
     sensors_event_t temp;
     icm.getEvent(&accel, &gyro, &temp); 
+
+    // TODO add this additional data and process it :)
+    // rollRate = (float)gyro.gyro.x/16.4;
+    // pitchRate = (float)gyro.gyro.y/16.4;
+
+    // accelX = (float)accel.acceleration.x/2048;
+    // accelY = (float)accel.acceleration.y/2048;
+    // accelZ = (float)accel.acceleration.z/2048;
+
+    // rollAngle = atan(accelY/sqrt(accelX*accelX + accelZ*accelZ)) * (1/(3.142/180)) + 1.67;
+    // pitchAngle =  atan(accelZ/sqrt(accelY*accelY + accelX*accelX)) * (1/(3.142/180)); 
 
     // Altimeter Data 
     float altitude = baro.getAltitude();
