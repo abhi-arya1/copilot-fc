b0e5741a17eaba84070bc3c43b8648d829c227ec
Abhi Arya <89806154+abhi-arya1@users.noreply.github.com>
2023-09-22 11:02:27 -0700

    reorganized config layout from most -> least used

--- launch_config.h
+++ launch_config.h
@@ -5,6 +5,9 @@
 // (Currently only supports Computer Mode 0)
 #define COMP_MODE 0
 
+// Set data-logging baud rate
+#define BAUD_RATE 115200
+
 // Set barometric "zero" pressure so the altitude readings correspond to the current altitude 
 #define BARO_ZERO_PRESSURE 1013.2
 
@@ -12,20 +15,19 @@
 // 1 -> Rocket activates channel to launch itself (Currently not supported)
 #define LAUNCH_METHOD 0
 
-// 0 -> TVC support disabled
-// 1 -> TVC support enabled (Currently not supported)
-#define TVC 0 
+// True or False based on if there is a backup pyro for the chute 
+#define PYRO_CHUTE_BACKUP_EN false
 
-// 0 -> Active Fin Control (AFC) disabled
-// 1 -> AFC enabled (Currently not supported)
-#define AFC 0 
+// Customize Pyro Channel Mappings to Digital I/O Ports
+#define PYRO_CHUTE 4
+/*note that PYRO_CHUTE_BACKUP_EN (above) must be set to "true" if you are using a backup channel */ 
+#define PYRO_CHUTE_BACKUP 9 
+/* other mappings to be used as necessary */
+// #define PYRO_LAUNCH 2 
+// #define PYRO_MISC 3 
 
-// 0 -> No Pyro Channel Backup 
-// 1 -> Pyro Channel Backup 
-#define PYRO_BACKUP_EN 0
+// true or false based on if Thrust Vector Control (TVC) is enabled or not (currently only "false" is supported)
+#define TVC false
 
-// Set data-logging baud rate
-#define BAUD_RATE 115200
-
-
- 
+// true or false based on if Active Fin Control (AFC) is enabled or not (currently only "false" is supported)
+#define AFC false 
